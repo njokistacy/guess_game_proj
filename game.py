b3ca2f6e384e2e8ebb76ac1e51b9e1382178478a
njokistacy <stacynjoks@gmail.com>
2023-02-16 09:24:32 +0300

    added hello message

---
+++ game.py
@@ -0,0 +1,21 @@
+#This program demonstrates a guessing game
+#STEPS
+#1.Import the random module
+from random import randint
+number=randint(10,50)
+#1.get user input
+player=input("What is your name?")
+print("Hello there " + player +"!")
+#generate a random number
+
+
+#2.Using a while loop limit the number of tries
+counter=0
+while counter<=5:
+    player_no=eval(input("Enter a random number between 10 and 50:"))
+    
+
+#2.generate a random number
+#3.using a while loop:check if user input is equal to generated number
+
+
